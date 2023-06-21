9322161a1d0a3a08b0f120f6821831eca3385baf
vincentrahadianutama <vincentutama38@smk.belajar.id>
2023-06-21 10:28:14 +0700

    second commit

--- factorial.php
+++ factorial.php
@@ -9,17 +9,31 @@ function factorialloop(int $value): int
     }
     return $total;
 }
-echo"This is using Loop".PHP_EOL;
-echo factorialloop(6).PHP_EOL;
+echo "This is using Loop" . PHP_EOL;
+echo factorialloop(6) . PHP_EOL;
 
 // Recursive Factorial
 function factorialrecursive(int $value): int
 {
     if ($value == 1) {
         return 1;
-    }else{
-        return $value * factorialrecursive($value-1);
+    } else {
+        return $value * factorialrecursive($value - 1);
     }
 }
-echo"This is using Recursive Function".PHP_EOL;
-echo factorialrecursive(5);
+
+// Recursive Looping
+function recursiveloop(int $value, int $limit)
+{
+    if ($value == $limit - 1) {
+        echo "Selesai" . PHP_EOL;
+    } else {
+        echo "Loop ke-$value" . PHP_EOL;
+        recursiveloop($value - 1, $limit + 0);
+    }
+}
+echo "This is using Recursive Function" . PHP_EOL;
+echo factorialrecursive(5) . PHP_EOL;
+
+// Print 100 to 15
+echo recursiveloop(100, 15);
